0ad2a4d9564012af7769ba3efbebad6c2c925fda
nikit <nikita.fedotov222@gmail.com>
2025-05-30 14:48:31 +0300

    solution

--- app/display.py
+++ app/display.py
@@ -3,18 +3,21 @@ from abc import ABC, abstractmethod
 
 class DisplayStrategy(ABC):
     @abstractmethod
-    def display(self, data: dict) -> None:
-        """Display the data in a specific format."""
+    def display(self, content: str) -> None:
         pass
 
 
 class ConsoleDisplay(DisplayStrategy):
-    def display(self, content: dict) -> None:
-        """Display data in the console."""
+    def display(self, content: str) -> None:
         print(content)
 
 
 class ReverseDisplay(DisplayStrategy):
-    def display(self, content: dict) -> None:
-        """Display data in reverse."""
+    def display(self, content: str) -> None:
         print(content[::-1])
+
+
+class PrintStrategy(ABC):
+    @abstractmethod
+    def print(self, title: str, content: str) -> None:
+        pass
